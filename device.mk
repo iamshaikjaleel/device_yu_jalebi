09ba7548d92f80edaa7ecdfb59e683788159a0bc
dianlujitao <dianlujitao@lineageos.org>
2018-05-26 12:26:39 +0530

    jalebi: Build dummy android.hidl.[base,manager]@1.0
    01-02 02:01:31.194   280   280 F libc    : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.0-service.widevine": library "android.hidl.base@1.0.so" not found
    01-02 02:01:31.194   280   280 F libc    : Fatal signal 6 (SIGABRT), code -6 in tid 280 (android.hardwar), pid 280 (android.hardwar)
    01-02 02:01:31.399   300   300 F DEBUG   : *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
    01-02 02:01:31.399   300   300 F DEBUG   : LineageOS Version: '15.1-20180525-UNOFFICIAL-a6000'
    01-02 02:01:31.400   300   300 F DEBUG   : Build fingerprint: 'Lenovo/Kraft-A6000/Kraft-A6000:5.0.2/LRX22G/Kraft-A6000_S061_160727:user/release-keys'
    01-02 02:01:31.400   300   300 F DEBUG   : Revision: '0'
    01-02 02:01:31.400   300   300 F DEBUG   : ABI: 'arm'
    01-02 02:01:31.400   300   300 F DEBUG   : pid: 280, tid: 280, name: android.hardwar  >>> /vendor/bin/hw/android.hardware.drm@1.0-service.widevine <<<
    01-02 02:01:31.400   300   300 F DEBUG   : signal 6 (SIGABRT), code -6 (SI_TKILL), fault addr --------
    01-02 02:01:31.402   300   300 F DEBUG   : Abort message: 'CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.0-service.widevine": library "android.hidl.base@1.0.so" not found'
    01-02 02:01:31.402   300   300 F DEBUG   :     r0 00000000  r1 00000118  r2 00000006  r3 00000008
    01-02 02:01:31.402   300   300 F DEBUG   :     r4 00000118  r5 00000118  r6 bee47774  r7 0000010c
    01-02 02:01:31.402   300   300 F DEBUG   :     r8 00000000  r9 00000000  sl acfaa010  fp 00000000
    01-02 02:01:31.402   300   300 F DEBUG   :     ip ad097f10  sp bee47760  lr ad0190f9  pc ad0175f8  cpsr 200f0030
    01-02 02:01:31.419   300   300 F DEBUG   :
    01-02 02:01:31.419   300   300 F DEBUG   : backtrace:
    01-02 02:01:31.419   300   300 F DEBUG   :     #00 pc 0005f5f8  /system/bin/linker (__dl_abort+63)
    01-02 02:01:31.419   300   300 F DEBUG   :     #01 pc 0001071f  /system/bin/linker (__dl___linker_init+2318)
    01-02 02:01:31.419   300   300 F DEBUG   :     #02 pc 00015454  /system/bin/linker (_start+4)
    
    Seems we need this now to run our widevine blobs
    
    NEEDED AF AFTER THIS
    
    -> device:
    iamsj7/android_device_yu_jalebi@ec4e1aa43f5a0c8500b0e9503f121a15861e3a10
    
    * Add this binderize drm hal to manifest
    dev-harsh1998/android_device_lenovo_a6000@ec33eb016391fc0eaccf54c861f69e47e01ec43e
    
    -> vendor:
    iamsj7/android_proprietary_vendor_yu@a1a33b2f37232d3ed6e040c84e0c9311601e0e96

--- device.mk
+++ device.mk
@@ -86,6 +86,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
